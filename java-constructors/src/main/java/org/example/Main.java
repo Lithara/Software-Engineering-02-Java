69defaf83ecb01cfdf432475b328951d030b216c
Lithara Perera <litharaperera2002@gmail.com>
2023-11-02 09:28:47 +0530

    modified

---
+++ java-constructors/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Employee objEmployee = new Employee();
+
+        System.out.println(objEmployee.Department);
+        System.out.println(objEmployee.name);
+        System.out.println(objEmployee.age);
+    }
+}
