914c42c069c022889498ebe63916f80a0babb347
Lithara Perera <litharaperera2002@gmail.com>
2023-11-03 20:56:33 +0530

    modified

---
+++ java-this/javaThis/src/main/java/com/mycompany/javathis/JavaThis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javathis;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class JavaThis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
