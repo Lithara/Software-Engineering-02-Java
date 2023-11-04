b746f4633fcecd25731eb3439e6e5902d9b4e18b
Lithara Perera <litharaperera2002@gmail.com>
2023-11-04 23:50:38 +0530

    modified

---
+++ java-this/src/main/java/org/example/Employee.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Employee {
+    String name;
+    int age;
+    String department;
+
+    public Employee(String name, int age, String department) {
+        this.name = name;
+        this.age = age;
+        this.department = department;
+    }
+}
