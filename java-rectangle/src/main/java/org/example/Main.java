8e5397d29f3302e7045ac9c09a5457a2c8fa5d7f
Lithara Perera <litharaperera2002@gmail.com>
2023-11-06 21:21:34 +0530

    modified

--- java-rectangle/src/main/java/org/example/Main.java
+++ java-rectangle/src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        Rectangle objRectangle1 = new Rectangle();
+        Rectangle objRectangle2 = new Rectangle(5,4);
+        Rectangle objRectangle3 = new Rectangle(10,6);
 
+        System.out.println("Rectangle 1 => Area : " + objRectangle1.getArea() + " Perimeter : " + objRectangle1.getPerimeter());
+        System.out.println("Rectangle 2 => Area : " + objRectangle2.getArea() + " Perimeter : " + objRectangle2.getPerimeter());
+        System.out.println("Rectangle 3 => Area : " + objRectangle3.getArea() + " Perimeter : " + objRectangle3.getPerimeter());
     }
 }
--- java-rectangle/src/main/java/org/example/Rectangle.java
+++ java-rectangle/src/main/java/org/example/Rectangle.java
@@ -1,4 +1,24 @@
 package org.example;
 
 public class Rectangle {
+    double length;
+    double width;
+
+    public Rectangle(){
+        this.length = 5;
+        this.width = 2;
+    }
+
+    public Rectangle(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getArea(){
+        return length * width;
+    }
+
+    public double getPerimeter(){
+        return 2*(length+width);
+    }
 }
