a530422fb17707b57ba2123e97bbafdea57a88ff
Lithara Perera <litharaperera2002@gmail.com>
2023-11-06 21:25:18 +0530

    modified

--- java-rectangle/src/main/java/org/example/Main.java
+++ java-rectangle/src/main/java/org/example/Main.java
@@ -9,5 +9,10 @@ public class Main {
         System.out.println("Rectangle 1 => Area : " + objRectangle1.getArea() + " Perimeter : " + objRectangle1.getPerimeter());
         System.out.println("Rectangle 2 => Area : " + objRectangle2.getArea() + " Perimeter : " + objRectangle2.getPerimeter());
         System.out.println("Rectangle 3 => Area : " + objRectangle3.getArea() + " Perimeter : " + objRectangle3.getPerimeter());
+
+        objRectangle1.length = 15;
+        objRectangle1.width = 10;
+
+        System.out.println("Rectangle 1 after changing values => Area : " + objRectangle1.getArea() + " Perimeter : " + objRectangle1.getPerimeter());
     }
 }
