f57dd563d5a0005c27efd7b1012c2c616df7fd96
Lithara Perera <litharaperera2002@gmail.com>
2023-11-20 23:47:26 +0530

    modified

---
+++ java-final/src/main/java/org/example/Child.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class Child extends Employee{
+
+}
---
+++ java-final/src/main/java/org/example/Employee.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Employee {
+    final int race = 10;
+    int laps = 5;
+
+    public final void run(){
+        System.out.println("I'm running");
+    }
+}
