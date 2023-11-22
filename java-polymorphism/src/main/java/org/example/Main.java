5827767e5bab632b1ac3355ebf3a171d93c50e1f
Lithara Perera <litharaperera2002@gmail.com>
2023-11-22 21:26:17 +0530

    modified

--- java-polymorphism/src/main/java/org/example/Main.java
+++ java-polymorphism/src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 
-class Bike{
+/*class Bike{
     void run(){
         System.out.println("bike is running");
     }
@@ -16,5 +16,47 @@ public class Main {
     public static void main(String[] args) {
         Bike objBike = new yamaha();
         objBike.run();
+    }
+}*/
+
+class Animal{
+    void eat(){
+        System.out.println("eating");
+    }
+}
+
+class Dog extends Animal{
+    void eat(){
+        System.out.println("Dog is running");
+    }
+}
+
+class BabyDog extends Dog{
+    void eat(){
+        System.out.println("Baby dog is running");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        /*Animal objAnimal1, objAnimal2, objAnimal3;
+
+        objAnimal1 = new Animal();
+        objAnimal1.eat();
+
+        objAnimal2 = new Dog();
+        objAnimal2.eat();
+
+        objAnimal3 = new BabyDog();
+        objAnimal3.eat();*/
+
+        Animal objAnimal1 = new Animal();
+        objAnimal1.eat();
+
+        Animal objAnimal2 = new Dog();
+        objAnimal2.eat();
+
+        Animal objAnimal3 = new BabyDog();
+        objAnimal3.eat();
     }
 }
