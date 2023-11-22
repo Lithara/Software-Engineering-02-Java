2621bc3922b4651432bec62c2c17fa451443268f
Lithara Perera <litharaperera2002@gmail.com>
2023-11-22 19:51:55 +0530

    modified

--- java-polymorphism/src/main/java/org/example/Main.java
+++ java-polymorphism/src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+class bike{
+    void run(){
+        System.out.println();
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
 
